2685b302a2c8bb6706498641ed58cee6fed9229c
LabRatsBBA <LabRatsBBA@gmail.com>
2023-08-13 09:38:09 +0400

    s

--- components/categories/index.js
+++ components/categories/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./styles.module.css";
-
+// categories
 function Categories({ categories }) {
   return (
     <div className={styles.categories}>
